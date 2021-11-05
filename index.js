8922c523d1dc0203e90cbc84846e6b90d442f361
Junier <junierba@yahoo.co.uk>
2021-11-05 09:13:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+/*
+function sayHello(name) {
+console.log('Hello' + name);
+}
+sayHello('Junier');
+
+
+
+console.log("Hello Winc Academy");
+
+    let name = 'Junier';
+
+    let greet = 'Hi ' + 'man!';
+
+    let total_1 = 8 + 2;
+    let total_2 = 8 * 2;
+    let total_3 = 8 / 2;
+    let total_4 = 8 % 2;
+    let total_5 = 8 + 2;
+
+    let leeftijd = 55;
+    console.log(typeof leeftijd);
+
+
+    leeftijd = '55';
+    console.log(typeof leeftijd);
+
+
+    console.log(name);
+
+    console.log(greet);
+
+    console.log(total_1);
+    console.log(total_2);
+    console.log(total_3);
+    console.log(total_4);
+    console.log(total_5);
+    */
